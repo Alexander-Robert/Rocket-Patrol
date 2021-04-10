cad65bcf501676704ee15c4bd3155644c6818345
Alexander Robert <allirobe@ucsc.edu>
2021-04-09 17:22:45 -0700

    explosion not working

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,7 +14,7 @@ class Play extends Phaser.Scene {
             frameWidth: 64,
             frameHeight: 32,
             startFrame: 0,
-            erndFrame: 9
+            endFrame: 9
         });
     }
     create() { //remember last things added in create are made first!!!!
@@ -74,15 +74,15 @@ class Play extends Phaser.Scene {
         //check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)){
             this.p1Rocket.reset();
-            this.ship03.reset();
+            this.shipExplode(this.ship03);
         }
         if(this.checkCollision(this.p1Rocket, this.ship02)){
             this.p1Rocket.reset();
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
         }
         if(this.checkCollision(this.p1Rocket, this.ship01)){
             this.p1Rocket.reset();
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
         }
     }
 
@@ -97,5 +97,18 @@ class Play extends Phaser.Scene {
             else {
                 return false;
             }
+    }
+
+    shipExplode(ship){
+        //temporarily hide ship
+        ship.alpha = 0;
+        //create explosion sprite at ship's position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0,0);
+        boom.anims.play('explode');
+        boom.on('animationcomplete', () => {
+            ship.reset();
+            ship.alpha = 1;
+            boom.destroy();
+        });
     }
 }
